0285663b49b7e1357cc629deb65a73e97c151ed0
RegisNobel <regisnobel95.com>
2024-03-04 21:36:24 -0500

    added mail sender service

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ End to end microservices devops project
     - finished devleoping basic app (front and backend services)
     - tested form and admin pages
     - pushed changes to github
+    
+
+3/4/2024
+    - added a branch on github
+    - develop the email sender service on new branch
+    - tested the email sender service
+    - pushed changes to github
--- auto_mail_service/app.py
+++ auto_mail_service/app.py
@@ -1,1 +1,38 @@
-print("testing mail sender")
+from flask import Flask, request, jsonify
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__)
+
+@app.route('/sendemail', methods=['POST'])
+def send_email():
+    data = request.json
+    sender = data.get('from', 'koudouva@gmail.com')
+    recipient = data['to']
+    subject = data['subject']
+    body = data['body']
+   
+
+    # Create message
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    
+
+
+    try:
+        # Setup server with Gmail
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender, 'xrya kzer hzzv cfhm') # You should use environment variables for credentials
+        server.sendmail(sender, recipient, msg.as_string())
+        server.quit()
+        return jsonify({'message': 'Email sent successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
